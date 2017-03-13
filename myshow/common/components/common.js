5cd0d59c07054168910dde359b97eb43fa89af43
YWZFrances <1595642024@qq.com>
2017-03-13 11:39:13 +0800

    全局基本搞定

--- myshow/common/components/common.js
+++ myshow/common/components/common.js
@@ -6,6 +6,15 @@ class Header extends Component{
 	constructor(props){
 		super(props)
 	}
+	shangyige(){
+		window.history.go(-1)
+	}
+	toCart(){
+		window.location.hash = "#/cart"
+	}
+	toMyOrder(){
+		window.location.hash = "#myorder"
+	}
 	render(){
 		return (
 			<div className = "header">
@@ -35,6 +44,11 @@ class Footer extends Component{
 	constructor(props){
 		super(props)
 	}
+	handleCilck(i,index){
+		console.log(222222)
+		var path = index==0?"/":index==1?"list":index==2?"cart":index==3?"show":"more"
+		window.location.hash="#/"+path
+	}
 	render(){
 		return (
 			<div className="footer">
@@ -53,4 +67,51 @@ Footer.defaultProps={
 	footerData:['首页','列表','购物车','我的','更多']
 }
 
-export { Header,Footer}
+//this.props.children 属性 他表示组件的所有子节点
+class SubHeader extends Component{
+	constructor (props){
+		super(props)
+	}
+	render(){
+		return(
+			<div className="sub-header">{this.props.children}</div>
+		)
+	}
+}
+
+class Content extends Component{
+	constructor(props){
+		super(props)
+	}
+	render(){
+		let contentStyle = {
+			"overflowY":this.props.hasIScroll?"hidden":"auto"
+		};
+		let contentClass = "content"
+			+(this.props.hasFooter?"has-footer":"")+(this.props.hasSubHeader?"has-sub-header":"");
+			//this.props.hasIScroll 如果需要iscroll就必须引入iscroll的结构
+			return (
+				<div className={contentClass} style={contentStyle}>
+					{this.props.hasIScroll?
+					<div className="scroll-wrap" ref="scrollWrap">
+						<div className="scroller">
+							{this.props.children}
+						</div>
+					</div>:this.props.children}
+				</div>
+			)
+	}
+	componentDidMount(){
+		//组件渲染完以后，获取scroll-wrap。创建iscroll
+		//如果需要iscroll再创建
+		this.props.hasIScroll && (this.myScroll = new IScroll(this.refs.scrollWraop))
+		//console.log(this.refs.scrollWrap)
+		//console.log(this.myScroll)
+	}
+	componentDidUpdate(){
+		//组件更新的时候，也更新iscroll
+		this.props.hasIScroll && this.myScroll.refresh()
+	}
+}
+
+export { Header,Footer,Content,SubHeader}
