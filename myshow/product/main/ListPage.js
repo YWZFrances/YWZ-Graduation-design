ac5cb0b22f73311f0c2b0a05d17617b84ee932a9
YWZFrances <1595642024@qq.com>
2017-03-15 17:42:01 +0800

    3.15

--- myshow/product/main/ListPage.js
+++ myshow/product/main/ListPage.js
@@ -1,1 +1,31 @@
-import {Header,Footer,Content,SubHeader} from "../../common"
+import {Header,Footer,Content,SubHeader} from "../../common/components/common"
+import ProductList from "../components/productList.js"//还没写？？？？怎么搞
+import {ScrollOptions} from "../../common/config/config.js"
+import React, {Components} from "react"
+import ReactIScroll from "react-iscroll"
+//商品分类列表
+class ClassList extends Component{
+	constructor(props){
+		super(props)
+	}
+	handleClick(id){
+		console.log(id);
+		this.props.changeClassID(id);
+	}
+	render(){
+		return (
+			<ul className="class-list">
+				{
+					this.props.classData.map((ele,i)=>
+						<li onClick={()=>this.handleClick(ele.classID)} key={i}>
+							{ele.className}
+						</li>
+					)
+				}
+			</ul>
+		)
+	}
+	ClassList.defaultProps={
+		classData:[]
+	};
+}
