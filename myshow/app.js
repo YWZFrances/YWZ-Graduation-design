0eae25ac63a172683f74b01bf18cad9bab5256cb
YWZFrances <1595642024@qq.com>
2017-03-23 17:05:23 +0800

    3.23

--- myshow/app.js
+++ myshow/app.js
@@ -9,6 +9,9 @@ import ListPage from "./product/main/listPage"
 import DetailPage from "./product/main/detailPage"
 import Information from "./product/main/proInformation"
 import CartPage from "./order/main/cartPage"
+import LoginPage from "./user/main/loginPage"
+import RegisterPage from "./user/main/registerPage"
+import MorePage from "./user/main/morePage"
 
 ReactDOM.render(<Router history={hashHistory}>
 	  		
@@ -17,6 +20,10 @@ ReactDOM.render(<Router history={hashHistory}>
 	<Route path="detail(/:goodsID)" component={DetailPage}  />
 	<Route path="information(/:goodsID)" component={Information}  />
 	<Route path="cart" component={CartPage}  />
+	<Route path="login" component={LoginPage}  />
+	<Route path="register" component={RegisterPage}  />
+	<Route path="more" component={MorePage}  />
+	
 </Router>,document.getElementById("root"));
 
 if (module.hot) {
